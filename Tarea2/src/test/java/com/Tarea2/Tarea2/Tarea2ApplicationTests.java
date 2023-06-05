a610654a5b84d80da3623403e72443bcf4226ba7
valverdeS7 <valverdetete17@gmail.com>
2023-06-05 16:25:16 -0600

    semana.3

---
+++ Tarea2/src/test/java/com/Tarea2/Tarea2/Tarea2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea2.Tarea2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
